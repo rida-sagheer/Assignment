b697e99c6ca18e2352d6206804fd9f9ee4c05be3
rida-sagheer <rida.sagheer2020@outlook.com>
2020-11-28 21:52:50 +0500

    Use of if conditional statements

---
+++ p30.java
@@ -0,0 +1,16 @@
+class p30{
+	public static void main(String args[])
+	{
+		int marks;
+		marks=50;
+		if(marks>=70){
+			System.out.println("PASSED");
+
+		}
+		else{
+			System.out.println("fail");
+			}
+	}
+
+
+}
